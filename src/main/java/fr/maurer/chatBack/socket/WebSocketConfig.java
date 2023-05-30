60f215d9311b230c1ebca3e1d433d342c4d7011f
CreativeWeb59 <delcri@free.fr>
2023-05-30 22:22:24 +0200

    modif websocket

--- src/main/java/fr/maurer/chatBack/socket/WebSocketConfig.java
+++ src/main/java/fr/maurer/chatBack/socket/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/private");
+        registry.enableSimpleBroker("/topic");
 //        registry.enableSimpleBroker("/chatroom", "/user");
 //        registry.setUserDestinationPrefix("/user");
     }
